f0c55b4c75322d9499fd8c87706011d65f10c4fd
Kendall Cole <kendall@veenome.com>
2014-08-10 15:19:01 -0400

    Update css

--- finished/urls.py
+++ finished/urls.py
@@ -4,5 +4,5 @@ urlpatterns = patterns('',
 	#url(r'^create_account/$', 'finished.views.create_account', name='finished_create_account'),
     url(r'^add_item/$', 'finished.views.add_item', name='finished_add_item'),
     url(r'^handle(.+)$', 'finished.views.handle', name='finished_handle'),
-    url(r'^delete_item/(.+)$', 'redirect_to', name='finished_delete_item'),
+    url(r'^delete_item/(.+)$', 'finished.views.delete_item', name='finished_delete_item'),
 )
